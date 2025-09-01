e33eb38dfc07f983fd0cb94d3a048e9058dd3a75
Elena_vinerbi <151653207+Elena-vin@users.noreply.github.com>
2025-09-01 11:46:18 +0200

    Update 12c.Summary_stats_CST.R

--- Code_for_Vinerbi_et_al/4. CST definition/12c.Summary_stats_CST.R
+++ Code_for_Vinerbi_et_al/4. CST definition/12c.Summary_stats_CST.R
@@ -1,9 +1,10 @@
 # Rscript
- 
-# Script to make a pie chart and bar plot for CST or subCST
+
+# script to check CST statistics and plot for cst or 
+# NB: for sbCSTs it is necessary to replace the CST label in the script
 
 # Author: Elena Vinerbi (elenavinerbi@cnr.it)
-# Last update: 03/02/2025
+# Last update: 27/08/2025
 
 # Dependence: R 4.4.1
 
@@ -16,106 +17,117 @@ library(patchwork)
 
 
 # Path
-setwd("/home/")
+setwd('/home/')
 getwd()
 
-# Import table 
-# Results of VALENCIA
-results_valencia <- read.table("Output_Valencia_CST.csv", sep = ",", header= T)
-View(results_valencia)
-head(results_valencia)[1:5]
+# Import table of CST/subCST (output by '12a.Prepare_table_for_VALENCIA_CST.R')
+df <- read.table('Output_Valencia.csv', sep = ',', header = T)
+head(df)[1:5]
 
-# Select specific column
-df <- select(results_valencia, sampleID, CST,subCST, score)
-head(df)
+# take samples, CST, CST_subgroup and score columns
+df1 <- select(df, sampleID, CST, subCST)
+head(df1)
 
-keep_IV <- function(x) {    # to remove subgroups of IV gropups
+# merge subCST of IV group in only one CST (CST IV)
+keep_IV <- function(x) {   
   sub("IV-[A-Z]", "IV", x)
 }
 
-df$CST <- sapply(df$CST, keep_IV)
-head(df)
+df1$CST <- sapply(df1$CST, keep_IV)
+head(df1)
 
-# All these steps must be repeated for the subCSt column
-# Create visits column (values are 1,2 ecc)
-df$Visits <- sub(".*_", "", df$sampleID)
-head(df)
+# Create a column with visit information
+df1$Visits <- sub(".*_", "", df1$sampleID)
+head(df1)
+dim(df1)
 
-# Create a percentage and counts of CST or subCST for each visits
-df_summary <- df %>%
-  group_by(CST) %>%  # Change with column subCST
-  summarise(total_score = sum(score)) %>%
-  mutate(percentage = total_score / sum(total_score) * 100)
-View(df_summary)
 
-# Specific palette for CST
-colors <- c('CST-I'="#FF8C6A",'CST-II'="#FFDA6A",'CST-III'="#CAD94A",'CST-IV'="#6AFF8A",'CST-V'= "#6A9EFF")
+# Calculate counts and  percentage of CST/subCST between all visits
+cst_counts <- table(df1$CST)
+head(cst_counts)
+
+cst_percent <- prop.table(cst_counts) * 100
+head(cst_percent)
+
+# Merge table
+tab <- merge(cst_counts, cst_percent, by =  'Var1')
+colnames(tab) <- c('CST', 'Counts', 'Percentage')
+head(tab)
+
+# trasform in data frame the table
+tab <- as.data.frame(tab)
+str(tab)
+
+# create a specific palette for CST
+colors <- c("#FF8C6A", "#FFDA6A", "#CAD94A", "#6AFF8A", "#6A9EFF")
 colors
 
 # Specific palette for subCST
-# subCST_colors <- c('subCST-IA' = "#FF8C6A", 'subCST-IB' = "#FFB48A", 'subCST-II' = "#FFDA6A", 'subCST-II' = "#FFE28A",  
+#   colors <- c('subCST-IA' = "#FF8C6A", 'subCST-IB' = "#FFB48A", 'subCST-II' = "#FFDA6A", 'subCST-II' = "#FFE28A",  
 #   'subCST-IIIA' = "#FFFF6A", 'subCST-IIIB' = "#FFFF8A",  
 #   'subCST-IVB' = "#6AFF8A", 'subCST-IVC0' = "#8AFFA5", 'subCST-IVC1' = "#AAFFBF", 'subCST-IVC3' = "#C2FFDB",  
 #   'subCST-V' = "#6A9EFF")
 
-# Pie chart
-P <- ggplot(df_summary, aes(x = "", y = total_score, fill = CST)) +  # Change with column subCST
+# pie chart plot
+P <- ggplot(tab, aes(x = "", y = Percentage, fill = CST)) +
   geom_bar(stat = "identity", width = 1) +
   coord_polar("y") +
   scale_fill_manual(values = colors) +
   theme_void() +
-  theme(plot.background = element_rect(fill = "white", color = NA),  
-        panel.background = element_rect(fill = "white", color = NA),  
+  theme(plot.background = element_rect(fill = "white", color = NA),
+        panel.background = element_rect(fill = "white", color = NA),
         legend.background = element_rect(fill = "white", color = NA),
-        legend.position = "none") + 
-  geom_text(aes(label = paste0(round(percentage, 1), "%")), 
-            position = position_stack(vjust = 0.7), 
-            color = "black", size = 5) 
+        legend.position = "none") +
+  geom_text(aes(label = paste0(round(Percentage, 1), "%")),
+            position = position_stack(vjust = 0.7),
+            color = "black", size = 5)
 
 P
-
 ggsave("Pie_chart_percentage.png", P, width = 12, height = 7, dpi = 300)
 
-# Calculate for each weeks the percentage of CST or subCST
-head(df)
+#Calculate percentage of CST for each visit
+head(df1)
+#
+# dim(subset(df1, Visits == '1'))
+# dim(subset(df1, Visits == '2'))
+# dim(subset(df1, Visits == '3'))
+# dim(subset(df1, Visits == '4'))
 
-df_percent <- df %>%
-  group_by(Visits, CST) %>%  # Change with column subCST
-  summarise(count = n(), .groups = 'drop') %>%
-  group_by(CST) %>%
-  mutate(percentage = count / sum(count) * 100)
-head(df_percent)
-View(df_percent)
+# calculate counts e percentage
+freq_table <- table(df1$Visit, df1$CST)
+head(freq_table)
 
-# Plot
-df_percent$CST <- ifelse(df_percent$CST == 'I', 'CST-I', df_percent$CST)  # Change with column subCST
-df_percent$CST <- ifelse(df_percent$CST == 'II', 'CST-II', df_percent$CST)  # Change with column subCST
-df_percent$CST <- ifelse(df_percent$CST == 'III', 'CST-III', df_percent$CST)  # Change with column subCST
-df_percent$CST <- ifelse(df_percent$CST == 'IV', 'CST-IV', df_percent$CST)  # Change with column subCST
-df_percent$CST <- ifelse(df_percent$CST == 'V', 'CST-V', df_percent$CST)  # Change with column subCST
-head(df_percent)
+percent_table <-prop.table(freq_table, margin = 1) * 100
+head(percent_table)
+
+# trasform table in data.frame
+percent_table <- as.data.frame(percent_table)
+colnames(percent_table) <- c('Phases','CST', 'Percentage')
+head(percent_table)
 
 # barplot
-P2 <- ggplot(df_percent, aes(x = Visits, y = percentage, fill = CST)) +
-      geom_bar(stat = "identity", position = 'dodge') +
-      labs(title = "", x = "", y = "Value (%)") +
-      theme_bw() +
-      scale_fill_manual(values = colors) +
-      ylim(0,40) +
-  scale_x_discrete(labels = c("1" = "F", "2" = "O", "3" = "EL", "4" = "LL")) +  
-  theme(axis.text = element_text(size = 7, family = 'sans'),  
-        axis.title = element_text(size = 7, family = 'sans'),
-        legend.text = element_text(size = 7, family = 'sans'),  
-        legend.title = element_text(size = 7, family = 'sans'))
-P2  
+P2 <- ggplot(percent_table, aes(x =Phases, y = Percentage, fill = CST)) +
+  geom_bar(stat = "identity", position = 'dodge') +
+  labs(title = "", x = "", y = "Value (%)") +
+  theme_bw() +
+  scale_fill_manual(values = colors) +
+  ylim(0,50) +
+  scale_x_discrete(labels = c("1" = "F", "2" = "O", "3" = "EL", "4" = "LL")) +
+  theme(axis.text.x = element_text(size = 15),
+        axis.text.y = element_text(size = 15),
+        axis.title = element_text(size = 15),
+        legend.text = element_text(size = 15),
+        legend.title = element_text(size = 15))
+P2
 
-ggsave("CST_across_visits.png", P2, width = 12, height = 7, dpi = 300)
+ggsave("CST_across_phases.png", P2, width = 12, height = 7, dpi = 300)
 
-# Create a panel
+
+# create a panel with two plots
 combined_plot <- P + P2 +
   plot_annotation(tag_levels = 'A')
 combined_plot
 
 #Save output
-ggsave("Combined_CST_distribution.png", combined_plot, width = 15, height = 7, dpi = 300)
+ggsave("Combined_CST_Panel.png", combined_plot, width = 15, height = 7, dpi = 300)
 
